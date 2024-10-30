6d275b93abc294af43cbaa078c64fec26809909e
ikanago <me@ikanago.dev>
2024-10-30 16:15:53 +0900

    Detect MeasurementTwice roughly

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,25 @@ fn main() {
     };
     let stmts = &module.body;
     for (i, stmt) in stmts.iter().enumerate() {
-        if let Some(name) = extract_call_name(stmt) {
+        if let Some(expr) = stmt.as_expr_stmt()
+            && let Some(call) = expr.value.as_call_expr()
+        {
             for other_stmt in stmts.iter().skip(i + 1) {
-                if let Some(other_name) = extract_call_name(other_stmt)
-                    && name.id == other_name.id
+                if let Some(other_expr) = other_stmt.as_expr_stmt()
+                    && let Some(other_call) = other_expr.value.as_call_expr()
+                    && let Some(func) = call.func.as_attribute_expr()
+                    && let Some(other_func) = other_call.func.as_attribute_expr()
+                    && let Some(instance_name) = func.value.as_name_expr()
+                    && let Some(other_instance_name) = other_func.value.as_name_expr()
+                    && let Some(target_qubit) = call.args[0].as_constant_expr()
+                    && let Some(other_target_qubit) = other_call.args[0].as_constant_expr()
+                    && let Some(target_qubit) = target_qubit.value.as_int()
+                    && let Some(other_target_qubit) = other_target_qubit.value.as_int()
+                    && instance_name.id == other_instance_name.id
+                    && func.attr == other_func.attr
+                    && target_qubit == other_target_qubit
                 {
-                    dbg!(name);
+                    println!("Found a duplicate call to {}", instance_name.id);
                 }
             }
         }
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-print(1)
-print(2)
-
-def f() -> int:
-    return 1
+from qiskit import QuantumCircuit
+circuit = QuantumCircuit(3, 3)
+circuit.h(0)
+circuit.cx(0, 1)
+circuit.cx(1, 2)
+circuit.measure(0, 0)
+circuit.measure(2, 2)
+circuit.measure(0, 1)
